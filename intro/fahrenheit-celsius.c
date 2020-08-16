85079463886d7e5501a8ec51fcac958e2c5fef99
nickherrig <neherrig@gmail.com>
2020-08-15 19:33:54 -0500

    fahrenheit to celcius func, introduces while loops, printing, variables, steps.

---
+++ intro/fahrenheit-celsius.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* print fahrenheit-celsius table
+   for 20, 40, ... 300 */
+
+main() {
+
+    int fahr, celcius;
+    int lower, upper, step; 
+
+    lower = 0;
+    upper = 300;
+    step =  20;
+
+    fahr =  lower;
+    while (fahr <= upper) {
+        celcius = 5 * (fahr-32) / 9;
+        printf("%d \t %d \n", fahr, celcius);
+        fahr = fahr + step;
+    }
+}
