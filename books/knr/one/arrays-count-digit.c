b90721e8695efbf4677aed8cace369db058ffa53
nickherrig <neherrig@gmail.com>
2020-12-06 20:21:51 -0600

    Counting whitespaces,tabs,etc

--- books/knr/one/arrays-count-digit.c
+++ books/knr/one/arrays-count-digit.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
 
-main()
+int main()
 {
 
-    int c, i, cntwhite, cntother;
+    int c, i, nwhite, nother;
     int cntdigit[10];
 
-    cntwhite = cntother = 0;
+    nwhite = nother = 0;
     for (i=0; i < 10; ++i)
         cntdigit[i] = 0;
 
     while((c = getchar()) != EOF)
         if (c > '0' || c <= '9')
             ++cntdigit[c-'0'];
+        else if (c == ' ' || c == '\n' || c == '\t')
+            ++nwhite;
+        else
+            ++nother;
 
     for (i = 0; i < 10; ++i)
         printf("%d   ", cntdigit[i]);
+    printf("white space: %d other: %d\n", nwhite, nother);
 }
