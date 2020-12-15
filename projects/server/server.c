1a9658b8af9923b13df95b2b5e61abc9c47150ec
nickherrig <neherrig@gmail.com>
2020-12-14 20:32:03 -0600

    Add open socket, print result

--- projects/server/server.c
+++ projects/server/server.c
@@ -2,30 +2,40 @@
 server.c -- a minimal webserver written
 */
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <sys/errno.h>
+#include <sys/socket.h>
+
 
 int main(int argc, char *argv[])
 {
-    struct addrinfo hints, *res;
+    struct addrinfo hints, *res, *p;
     int status, sockfd;
 
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
     if ((status = getaddrinfo(NULL, "8080", &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-        return 1;
+        exit(1);
     }
 
+    for(p = res; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
+    }
+
+    printf("socket: %i\n", sockfd);
+    freeaddrinfo(res);
     return 0;
 
 }
