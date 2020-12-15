c8909d7d1a1a43fe09b3e4a650250df552d5ee56
nickherrig <neherrig@gmail.com>
2020-12-15 08:17:17 -0600

    Add socketopt and bind

--- projects/server/server.c
+++ projects/server/server.c
@@ -9,33 +9,49 @@ server.c -- a minimal webserver written
 #include <sys/socket.h>
 #include <netdb.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 
-int main(int argc, char *argv[])
+int main(void)
 {
-    struct addrinfo hints, *res, *p;
+    struct addrinfo hints, *servinfo, *p;
     int status, sockfd;
-
+    int yes=1;
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    if ((status = getaddrinfo(NULL, "8080", &hints, &res)) != 0) {
+    if ((status = getaddrinfo(NULL, "8080", &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         exit(1);
     }
 
-    for(p = res; p != NULL; p = p->ai_next) {
+    for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("server: socket");
             continue;
         }
+    
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+            perror("server: socket options");
+            exit(1);
+        }
+
+        if ((bind(sockfd, p->ai_addr, p->ai_addrlen)) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+        
+        break;
     }
 
-    printf("socket: %i\n", sockfd);
-    freeaddrinfo(res);
+    freeaddrinfo(servinfo);
+
+    close(sockfd);
+
     return 0;
 
 }
